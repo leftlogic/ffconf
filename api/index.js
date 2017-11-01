bceaa8c5fa86ce9265ccff56f3d1dec8072b66ce
Remy Sharp <remy@remysharp.com>
2017-11-01 14:37:55 +0000

    fix: bad request on root API query

--- api/index.js
+++ api/index.js
@@ -8,27 +8,27 @@ const client = new GraphQLClient(endpoint, { headers: {} });
 
 module.exports = router;
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   client
     .request(
       `{
-  allEvents() {
-    sessions {
-      title
-      description
-    }
-  }
-    }`
+          allEvents {
+            year
+            url
+            sessions {
+              title
+              description
+            }
+          }
+      }`
     )
     .then(results => {
-      res.status(200).json(results);
+      res.status(200).json(results.allEvents);
     })
-    .catch(e => {
-      res.status(400).json(e.message);
-    });
+    .catch(next);
 });
 
-router.get('/:year/:slug?', (req, res) => {
+router.get('/:year/:slug?', (req, res, next) => {
   const filter = [];
   const { year, slug = false } = req.params;
   filter.push(`year: ${year}`);
@@ -38,14 +38,21 @@ router.get('/:year/:slug?', (req, res) => {
   client
     .request(
       `{
-  allEvents(filter:{ ${filter.join(' ')} }) {
-    sessions {
-      title
-      description
-    }
-  }
-    }`
+        allEvents(filter:{ ${filter.join(' ')} }) {
+          sessions {
+            title
+            description
+          }
+        }
+      }`
     )
     .then(results => res.status(200).json(results.allEvents[0].sessions))
-    .catch(e => res.status(400).json(e.response || e.message));
+    .catch(next);
+});
+
+router.use((error, req, res, next) => {
+  if (error.response) {
+    error = error.response;
+  }
+  res.status(400).json(error);
 });
