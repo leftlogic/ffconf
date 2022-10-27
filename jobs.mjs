956be98da16d541b047c30a1af9b9cbd47c39b16
Remy Sharp <remy@remysharp.com>
2022-10-27 19:32:48 +0100

    wip: jobs build

---
+++ jobs.mjs
@@ -0,0 +1,66 @@
+import { parse } from 'csv-parse/sync';
+import { writeFileSync } from 'fs';
+import { resolve } from 'path';
+import fetch from 'node-fetch';
+import slugify from 'slugify';
+
+const url =
+  'https://docs.google.com/spreadsheets/d/e/2PACX-1vSW2DHW7Optt8J5aRYmYyHF3Vkx5ixbgnX8ZVcDBXNeE_0WRkcigY3PYVbQUfpvj_yrtNdAvK4jjbW3/pub?output=csv';
+
+async function main() {
+  const res = await fetch(url);
+  const text = await res.text();
+  const records = parse(text, {
+    columns: [
+      'timestamp',
+      'email',
+      'title',
+      'description',
+      'city',
+      'country',
+      'type',
+      'company',
+      'url',
+      'from',
+      'promote',
+      'approved',
+    ],
+    skip_empty_lines: true,
+    from: 2,
+  });
+
+  console.log(records);
+
+  writeFileSync(
+    resolve('src', '_data', 'jobs.json'),
+    JSON.stringify(
+      records
+        .map((_) => {
+          if (_.from) {
+            // _.from = new Date(_.from);
+          } else {
+            _.from = _.timestamp;
+          }
+          _.slug = slugify(`${_.company} - ${_.title}`, { lower: true });
+          if (!_.approved || _.approved.toLowerCase() === 'n') {
+            _.approved = false;
+          } else {
+            _.approved = true;
+          }
+
+          return _;
+        })
+        .filter((_) => {
+          if (!_.approved) {
+            return false;
+          }
+
+          return true;
+
+          // also if expired
+        })
+    )
+  );
+}
+
+main().catch((E) => console.log(E));
