05b0ae550d4635e18f8eb859c3d42874d8ab590f
Remy Sharp <remy@remysharp.com>
2022-11-02 17:14:42 +0000

    fix: simple redirects and 404

--- redirects.js
+++ redirects.js
@@ -8,9 +8,9 @@ const Talks = require('./src/_data/talks');
 const main = async () => {
   const files = await readdir(__dirname + '/redirects');
   const contents = await Promise.all(
-    files.map(filename => {
+    files.map((filename) => {
       return readFile(__dirname + '/redirects/' + filename, 'utf8').then(
-        url => {
+        (url) => {
           return `/${filename.trim()} ${url.trim()} 302`;
         }
       );
@@ -25,14 +25,15 @@ const main = async () => {
     '/graphql https://ffconf-graphql.isthe.link/ 200',
     '/api /api/index.json 200',
     '/api/help /api/help/index.json 200',
-    ...talks.map(_ => {
+    ...talks.map((_) => {
       years.add(_.event.year);
       return `/api/session/${_.slug} /api/session/${_.slug}/index.json 200`;
     }),
     ...Array.from(years).map(
-      year => `/api/event/${year} /api/event/${year}/index.json 200`
+      (year) => `/api/event/${year} /api/event/${year}/index.json 200`
     ),
-    '/api/* /api/404.json 404'
+    '/api/* /api/404.json 404',
+    '/* /404 404'
   );
 
   await writeFile(__dirname + '/dist/_redirects', contents.join('\n'));
